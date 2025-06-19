3a8afb101c21655202bf689a51ab7d06363026d1
TheKevin2334 <162564157+TheKevin2334@users.noreply.github.com>
2025-06-19 11:44:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',                   // Required for Render to access it
+    port: process.env.PORT || 5173,    // Use Render-assigned port or fallback locally
+  }
 });
