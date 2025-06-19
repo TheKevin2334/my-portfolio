3bed7aadfef9bffec9c36ec14a820274a4dfc30c
TheKevin2334 <162564157+TheKevin2334@users.noreply.github.com>
2025-06-19 11:47:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',                   // Required for Render to access it
-    port: process.env.PORT || 5173,    // Use Render-assigned port or fallback locally
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: ['my-portfolio-ebak.onrender.com'], // 👈 Add this line
   }
 });
