eb404a5070698d687c272b4abe8eb0b0532ff8dc
TheKevin2334 <162564157+TheKevin2334@users.noreply.github.com>
2025-06-19 11:55:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    allowedHosts: ['my-portfolio-ebak.onrender.com'], // 👈 Add this line
-  }
 });
